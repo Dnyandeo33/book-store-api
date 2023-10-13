d9993bd36b2d2981be6c3152ebd34d1cc96ac2ac
Dnyandeo Waghunde <dnyanu.waghunde@gmail.com>
2023-10-14 00:33:22 +0200

    added ejs file and data

--- controllers/books.js
+++ controllers/books.js
@@ -1,14 +1,49 @@
 let books = [
     {
         id: '1',
-        Name: 'JavaScript',
-        Author: 'xyz abc'
+        name: 'Life of Pi',
+        author: 'Yann Martel',
+        description: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt exercitationem iste,
+        voluptatum, quia explicabo laboriosam rem adipisci voluptates cumque, veritatis atque
+        nostrum corrupti ipsa asperiores harum? Dicta odio aut hic.`,
+        src: 'https://www.movienewsletters.net/photos/070671R1.jpg',
     },
     {
         id: '2',
-        Name: 'Java',
-        Author: 'abc xyz'
-    }
+        name: 'The Scarlet Pimpernel',
+        author: 'Baroness Orczy',
+        description: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt exercitationem iste,
+        voluptatum, quia explicabo laboriosam rem adipisci voluptates cumque, veritatis atque
+        nostrum corrupti ipsa asperiores harum? Dicta odio aut hic.`,
+        src: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSoPX_I0h4v27_nFFRWSYEE_czlnWtEBKAp4FXu4uM2Qb4j4Oxe'
+    },
+    {
+        id: '3',
+        name: 'Fourth Wing',
+        author: 'Baroness Orczy',
+        description: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt exercitationem iste,
+        voluptatum, quia explicabo laboriosam rem adipisci voluptates cumque, veritatis atque
+        nostrum corrupti ipsa asperiores harum? Dicta odio aut hic.`,
+        src: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTMGygN6vmJYLi9nkL769kQfNDg3kt-zfrb1AQRkAnvtiUysGdh'
+    },
+    {
+        id: '4',
+        name: 'Things We Never Got Over',
+        author: 'Lucy Score',
+        description: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt exercitationem iste,
+        voluptatum, quia explicabo laboriosam rem adipisci voluptates cumque, veritatis atque
+        nostrum corrupti ipsa asperiores harum? Dicta odio aut hic.`,
+        src: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1641629293i/60060431.jpg"
+    },
+    {
+        id: '5',
+        name: 'Be Useful: Seven Tools for Life',
+        author: 'Arnold Schwarzenegger',
+        description: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt exercitationem iste,
+        voluptatum, quia explicabo laboriosam rem adipisci voluptates cumque, veritatis atque
+        nostrum corrupti ipsa asperiores harum? Dicta odio aut hic.`,
+        src: 'https://m.media-amazon.com/images/I/91H1uVS7QxL.jpg'
+    },
 ];
 
 const getBooksById = (id) => {
@@ -17,26 +52,27 @@ const getBooksById = (id) => {
 
 const routeHandler = {
     getBooks: (req, res) => {
-        res.status(200).json(books);
+        res.status(200).render('books', { books: books });
     },
 
     getBookById: (req, res) => {
         const { id } = req.params;
         const bookExist = getBooksById(id);
         if (bookExist) {
-            res.status(200).json(bookExist);
+            res.status(200).render('book', { bookExist: bookExist });
         } else {
             res.status(200).json({ message: `Book doesn't exist with id ${id}` });
         }
     },
 
     postBook: (req, res) => {
-        const { Name, Author } = req.body;
+        const { name, author, src } = req.body;
 
         const newBook = {
             id: String(books.length + 1),
-            Name: Name,
-            Author: Author
+            name: name,
+            author: author,
+            src: src
         }
         books.push(newBook)
         res.status(201).json(newBook)
@@ -44,13 +80,13 @@ const routeHandler = {
 
     updateBook: (req, res) => {
         const { id } = req.params;
-        const { Name, Author } = req.body;
+        const { name, author, src } = req.body;
 
         const bookExist = getBooksById(id);
         if (bookExist) {
             books.forEach((book, index) => {
                 if (book.id === id) {
-                    const updateBook = { id: id, Name: Name, Author: Author }
+                    const updateBook = { id: id, name: name, author: author, src: src }
                     books[index] = updateBook;
                     res.status(200).json(updateBook)
                 }
--- server.js
+++ server.js
@@ -1,16 +1,39 @@
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
 import dotenv from 'dotenv';
 import express from 'express';
+
 import bookRoutes from './routes/books.js';
 
 dotenv.config();
 const PORT = process.env.PORT;
 
+// set absolute path
+const __fileName = fileURLToPath(import.meta.url);
+const PATH = dirname(__fileName);
+
 const app = express();
+
+// set template engin
+app.set('view engine', 'ejs');
+app.set('views', path.join(PATH, 'views'))
 
 // parse body
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
 
+// static folder path
+app.use(express.static(path.join(PATH, 'public')))
+
+// routes
 app.use('/api/books', bookRoutes);
+
+// 404 route
+app.use((req, res) => {
+    res.status(200).render('404', { message: `Page doesn't fond` })
+
+})
 
 app.listen(PORT, () => {
     console.log(`server running on ${PORT}`);
